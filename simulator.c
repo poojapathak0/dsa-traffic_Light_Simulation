0de032cc54e7c769609bd1f3f90aaa61adbea7d6
pathak <poojapathak200533@gmail.com>
2025-02-04 16:21:31 +0545

    idk

--- simulator.c
+++ simulator.c
@@ -1,95 +1,67 @@
-#include "include/SDL.h"
+#include <SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <time.h>
+#include <unistd.h>
 
+#define NUM_LANES 4
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
-#define NUM_ROADS 4
-#define MAX_VEHICLES 20
 
 typedef struct {
-    int waitingVehicles;
-    bool isPriority;
+    int vehicles;
+    int priority;
 } Lane;
 
-Lane lanes[NUM_ROADS];
+Lane lanes[NUM_LANES];
+const char *lane_files[NUM_LANES] = {"laneA.txt", "laneB.txt", "laneC.txt", "laneD.txt"};
 
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
-
-void initializeSDL() {
-    SDL_Init(SDL_INIT_VIDEO);
-    window = SDL_CreateWindow("Traffic Light Simulation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-}
-
-void cleanupSDL() {
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-}
-
-void renderTrafficLights() {
-    // Render traffic lights for each road
-    for (int i = 0; i < NUM_ROADS; i++) {
-        // Set color based on light state
-        SDL_SetRenderDrawColor(renderer, lanes[i].isPriority ? 0 : 255, lanes[i].isPriority ? 255 : 0, 0, 255); // Green for priority, red otherwise
-        SDL_Rect lightRect = { i * (WINDOW_WIDTH / NUM_ROADS), 50, (WINDOW_WIDTH / NUM_ROADS) - 10, 100 };
-        SDL_RenderFillRect(renderer, &lightRect);
-    }
-}
-
-void processTraffic() {
-    // Check for priority conditions
-    for (int i = 0; i < NUM_ROADS; i++) {
-        if (lanes[i].waitingVehicles > 10) {
-            lanes[i].isPriority = true;
-        } else {
-            lanes[i].isPriority = false;
-        }
-    }
-
-    // Simulate vehicle passing
-    for (int i = 0; i < NUM_ROADS; i++) {
-        if (lanes[i].isPriority && lanes[i].waitingVehicles > 0) {
-            lanes[i].waitingVehicles--;
+void read_traffic_data() {
+    for (int i = 0; i < NUM_LANES; i++) {
+        FILE *file = fopen(lane_files[i], "r");
+        if (file) {
+            fscanf(file, "%d", &lanes[i].vehicles);
+            fclose(file);
         }
     }
 }
 
-void render() {
+void update_priority() {
+    for (int i = 0; i < NUM_LANES; i++) {
+        if (lanes[i].vehicles > 10) {
+            lanes[i].priority = 1;
+        } else if (lanes[i].vehicles < 5) {
+            lanes[i].priority = 0;
+        }
+    }
+}
+
+void render_traffic(SDL_Renderer *renderer) {
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
-    renderTrafficLights();
+
+    for (int i = 0; i < NUM_LANES; i++) {
+        SDL_Rect rect = {i * 200, 0, 200, lanes[i].vehicles * 20};
+        SDL_SetRenderDrawColor(renderer, lanes[i].priority ? 255 : 0, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &rect);
+    }
+
     SDL_RenderPresent(renderer);
 }
 
-int main(int argc, char* argv[]) {
-    initializeSDL();
+int main() {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("Traffic Simulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    // Initialize lanes
-    for (int i = 0; i < NUM_ROADS; i++) {
-        lanes[i].waitingVehicles = rand() % MAX_VEHICLES; // Random initial vehicles
-        lanes[i].isPriority = false;
+    while (1) {
+        read_traffic_data();
+        update_priority();
+        render_traffic(renderer);
+        SDL_Delay(1000); // Update every 1 second
     }
 
-    bool running = true;
-    SDL_Event event;
-
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                running = false;
-            }
-        }
-
-        processTraffic();
-        render();
-        SDL_Delay(1000); // Update every second
-    }
-
-    cleanupSDL();
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
--- traffic_generator.c
+++ traffic_generator.c
@@ -1,37 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <unistd.h>
 
-#define NUM_ROADS 4
+#define NUM_LANES 4
 #define MAX_VEHICLES 20
 
-int lanes[NUM_ROADS];
+void generate_traffic() {
+    const char *lane_files[NUM_LANES] = {"laneA.txt", "laneB.txt", "laneC.txt", "laneD.txt"};
+    srand(time(NULL));
 
-void generateVehicles() {
-    for (int i = 0; i < NUM_ROADS; i++) {
-        if (lanes[i] < MAX_VEHICLES) {
-            lanes[i] += rand() % 3; // Add 0 to 2 vehicles randomly
-            if (lanes[i] > MAX_VEHICLES) {
-                lanes[i] = MAX_VEHICLES; // Cap at max vehicles
+    while (1) {
+        for (int i = 0; i < NUM_LANES; i++) {
+            FILE *file = fopen(lane_files[i], "w");
+            if (file) {
+                int vehicles = rand() % MAX_VEHICLES;
+                fprintf(file, "%d\n", vehicles);
+                fclose(file);
             }
         }
+        sleep(1); // Wait for 1 second before generating new traffic
     }
 }
 
 int main() {
-    srand(time(NULL));
-
-    // Initialize lanes
-    for (int i = 0; i < NUM_ROADS; i++) {
-        lanes[i] = 0; // Start with 0 vehicles
-    }
-
-    while (1) {
-        generateVehicles();
-        printf("Current vehicles in lanes: A: %d, B: %d, C: %d, D: %d\n", lanes[0], lanes[1], lanes[2], lanes[3]);
-        sleep(1); // Generate vehicles every second
-    }
-
+    generate_traffic();
     return 0;
 }
